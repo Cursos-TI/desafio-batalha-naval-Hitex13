bc4c631c56cdfa18166214f2a7eeca1f004bca00
Vinicius de Andrade Sampaio <cobracaptali@gmail.com>
2025-05-21 18:52:20 +0000

    Nivel Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,49 +1,41 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define Linha 10
+#define Coluna 10
 
 int main(){
 
-    char Linha[10] ={'A','B','C','D','E','F','G','H','I','J'} ;
-    int Naval[10][10];
-
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            Naval[i][j] = 0;
-        }
-    }
-
-    Naval[5][6] = 3;
-    Naval[5][7] = 3;
-    Naval[5][8] = 3;
-
-    Naval[2][1] = 3;
-    Naval[1][1] = 3;
-    Naval[3][1] = 3;
-    for (int q = 0; q < 1; q++)
-    {
-        for (int l = 0; l < 10; l++)
-        {
-            printf(" |%c|",Linha[l]);
-        }
-
-        for ( int f = 0; f < 10; f++)
-        {
-            printf("\n%d|",f);
-            for (int h = 0; h < 10; h++)
-            {
-                //printf("%d ",Naval[f][h]);
-                printf("%d   ",Naval[h][f]);
-            }
-            printf("\n");
-        }
-        
-        
-    }
+int Matriz[Linha][Coluna];
+char Matriz2[10]; 
+for (int i = 0; i < 10; i++)
+{
+    Matriz2[i]='A'+ i;
     
-return 0;
+    printf("    |%c| ", Matriz2[i]);
+}
 
-} 
+for (int i = 0; i < Linha; i++)
+{
+    printf("\n%d| ",i);
+    for  (int j = 0; j < Coluna; j++)
+    {
+        Matriz[i][j] = 0;
+        Matriz[5][6] = 3;
+        Matriz[5][7] = 3;
+        Matriz[5][8] = 3;
 
+        Matriz[2][1] = 3;
+        Matriz[1][1] = 3;
+        Matriz[3][1] = 3; 
+
+        Matriz[9][0] = 3;
+        Matriz[8][1] = 3;
+        Matriz[7][2] = 3;
+        printf("  %d     ", Matriz[i][j]);
+    }
+   
+}
+
+
+
+}
